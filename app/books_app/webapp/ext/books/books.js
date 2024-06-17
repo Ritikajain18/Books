db11d9426c63562db9ae3da61820e7a4ba90ca93
Ritika Jain <ritika.jain02@infosys.com>
2024-06-17 11:48:19 +0000

    added comment

--- app/books_app/webapp/ext/books/books.js
+++ app/books_app/webapp/ext/books/books.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/m/MessageToast"
 ], function(MessageToast) {
     'use strict';
-
+// comment added
     return {
         // create button added from open guided development
         // onInit: function(oEvent) {
